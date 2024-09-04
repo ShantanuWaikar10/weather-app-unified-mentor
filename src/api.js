ac94316f2fa8cf3d7c2c498c443407a6a867aa72
Shantanu Waikar <shantanuwaikar10@gmail.com>
2024-09-04 13:38:46 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,16 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b6c03ab9c4msh72a7aa4bcf7f64fp1f8a1djsn44f3d81da082',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = '5fba667e2abc30105a31cbb0abdfb691'
+
+
